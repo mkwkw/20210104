f032a56ccd3c34b0ccf82656f44d0e26e75cbb13
mkwkw <mkkw2047@gmail.com>
2021-01-05 21:28:42 +0900

    카운팅 정렬

--- Project6/src/p5.java
+++ Project6/src/p5.java
@@ -19,18 +19,19 @@ public class p5 {
 		for(int i=0; i<n; i++) {
 			
 			arr[i]= Integer.parseInt(br.readLine());
-			
+			arr1[arr[i]]++;
 		}
+		
 		
 		for(int i=1; i<10001; i++) {
 			while(arr1[i]>0) {
 				sb.append(i).append("\n");
 				arr1[i]--;
 			}
-			System.out.println(sb);
+			
 			
 		}
-		
+		System.out.println(sb);
 		br.close();
 
 	}
